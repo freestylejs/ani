86ae8eba93f157112f4da2e9dd93f4ca8493909e
Danpa_cho <danpa725@cau.ac.kr>
2025-11-15 02:25:51 +0900

    feat(ani): Add LoopNode and loop function for repeating animations

---
+++ packages/core/src/ani/nodes/loop.ts
@@ -0,0 +1,55 @@
+import type { TimingFunction } from '~/timing'
+import type { Groupable } from '../timeline'
+import type { AnimationId, AnimationNode } from './base'
+import { CompositionNode, type CompositionPlan } from './composition'
+
+/**
+ * Composition node that repeats a child animation node a specified number of times.
+ */
+export class LoopNode<G extends Groupable> extends CompositionNode<
+    readonly AnimationNode<G>[]
+> {
+    readonly type = 'LOOP'
+    readonly duration: number
+    readonly count: number
+    readonly child: AnimationNode<G>
+
+    constructor(
+        child: AnimationNode<G>,
+        loopCount: number,
+        timing?: TimingFunction,
+        id?: AnimationId
+    ) {
+        super([child], timing, id)
+        this.child = child
+        this.count = loopCount
+        this.duration = child.duration * loopCount
+    }
+
+    public construct(
+        plan: CompositionPlan<readonly AnimationNode<G>[]>,
+        startTime: number
+    ): void {
+        let currentTime = startTime
+        for (let i = 0; i < this.count; i++) {
+            this.child.construct(plan, currentTime)
+            currentTime += this.child.duration
+        }
+    }
+}
+
+/**
+ * Repeats a child animation node a specified number of times.
+ * @param child The animation node to repeat.
+ * @param loopCount The number of times to repeat the child node.
+ * @param timing loop timing function.
+ * @param id Optional ID for the node.
+ */
+export function loop<G extends Groupable>(
+    child: AnimationNode<G>,
+    loopCount: number,
+    timing?: TimingFunction,
+    id?: AnimationId
+): LoopNode<G> {
+    return new LoopNode(child, loopCount, timing, id)
+}
