47f4fa84d87451bf320e5693001431615eeb4dd8
Danpa_cho <danpa725@cau.ac.kr>
2025-11-22 21:37:52 +0900

    move: core engine

---
+++ packages/core/src/ani/core/engine.ts
@@ -0,0 +1,60 @@
+import type {
+    SegmentDefinition,
+    SegmentState,
+} from '~/ani/core/interface/core_types'
+import type { TimingFunction } from '~/timing'
+import { isEndOfAnimation } from '~/utils/time'
+
+/**
+ * Calculates the animated values for a single segment at a specific local time.
+ *
+ * @param localTime Time elapsed within this specific segment (from 0 to duration).
+ * @param segmentDef Target segment def
+ * @parma dt Delta time
+ * @returns Calculated values and whether the segment is complete.
+ */
+export function calculateSegmentState(
+    localTime: number,
+    segmentDef: SegmentDefinition,
+    dt: number = 0
+): SegmentState {
+    const t = Math.max(0, Math.min(localTime, segmentDef.duration))
+
+    const animeValues: Array<number> = []
+    let allComplete = true
+
+    const isMultipleTiming = Array.isArray(segmentDef.timing)
+
+    if (
+        isMultipleTiming &&
+        (segmentDef.timing as Array<TimingFunction>).length !==
+            segmentDef.from.length
+    ) {
+        throw new TypeError(
+            `[calculateSegmentState] timing does not correctly set. It requires multiple timing for ${segmentDef.from}, but received ${segmentDef.timing}`
+        )
+    }
+
+    for (let i = 0; i < segmentDef.from.length; i++) {
+        const timingFunction: TimingFunction = isMultipleTiming
+            ? (segmentDef.timing as TimingFunction[])[i]!
+            : (segmentDef.timing as TimingFunction)
+
+        const animeResponse = timingFunction.step(t, {
+            dt: dt,
+            from: segmentDef.from[i]!,
+            to: segmentDef.to[i]!,
+            duration: segmentDef.duration,
+        })
+
+        animeValues.push(animeResponse.value)
+        if (!animeResponse.endOfAnimation) {
+            allComplete = false
+        }
+    }
+
+    return {
+        values: animeValues,
+        isComplete: allComplete || isEndOfAnimation(t, segmentDef.duration),
+    }
+}
